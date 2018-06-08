2a594fb0b046f3ba554cd2d863bc5df50cfdfb7e
Tony Chheng <tonychheng@MacBook-Pro-de-Tony.local>
2018-06-08 02:00:48 -0400

    Init project

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="robots" content="index,follow" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+		<!-- Favicon -->
+		<!-- <link rel="shortcut icon" href="img/favicon.ico" /> -->
+		<title>Modèle dev</title>
+
+		<!-- Google font -->
+		<!-- Open Sans -->
+		<!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'> -->
+
+		<!-- Roboto, Regular : 400, Bold : 700 -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/main.css" />
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<div class="parent_box">
+
+			<div class="child_box content">
+				<div class="row">
+
+					<?php
+
+						echo 'Hello World!';
+
+					?>
+
+				</div>
+			</div>
+
+		</div>
+
+		<!-- ///////////////////////////// -->
+		<!-- JQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+
+		<!-- Main js -->
+		<!-- <script src="js/main.js"></script> -->
+	</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+var personnage = {
+  prenom : 'Tony',
+  nom : 'Chheng',
+  age : 26
+};
+
+console.log(personnage.age);
+
+if (personnage.age >= 30) {
+  console.log('Vous êtes un jeune homme');
+
+} else if (personnage.age == 26) {
+  console.log("C'est moi!");
+
+} else {
+  console.log('Vous êtes vieux');
+}
+
+
+var i = 10; //integer
+    i = "World's" + i; //string with double quote don't need backslash and can read variable i
+
+console.log(i);
